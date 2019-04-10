e368eae1fcdb8dd197af576ab96a4e774b0e1c47
Guilherme Dias <guilhermegdf2@hotmail.com>
2019-04-10 20:29:08 -0300

    first commit

---
+++ build/precache-manifest.2d2059842d9e857bd2b53fb5340415fa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "54fb26c35b3297bb3d13",
+    "url": "/static/js/main.47a652af.chunk.js"
+  },
+  {
+    "revision": "cbb9a96f7e81d4b0c010",
+    "url": "/static/js/2.e80e55e1.chunk.js"
+  },
+  {
+    "revision": "88716ff18136f66e4b16159dd4635e38",
+    "url": "/index.html"
+  }
+];
